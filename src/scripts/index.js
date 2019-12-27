15b3d3dea0870c0ce9d5f37170026cf40001e54c
Владислав Данилов <danilov@RIT-L005.rit.va>
2019-12-27 13:09:46 +0300

    add header and promo slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
 import '../styles/index.scss';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import "./navbar.js";
+import "./slider.js";
 
 console.log('webpack starterkit');
